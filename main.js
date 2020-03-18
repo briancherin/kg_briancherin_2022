dbefbefcbbe9636ca5f76489e5048edf2c30f5f7
Brian Cherin <brian.cherin@gmail.com>
2020-03-18 11:15:30 -0400

    Create function to check mapping existence

---
+++ main.js
@@ -0,0 +1,29 @@
+
+function mappingExists(str1, str2) {
+	const len1 = str1.length;
+	const len2 = str2.length;
+	
+	//There can not be a mapping between strings of non-equal length
+	if (len1 != len2) {
+		return false;
+	}
+	
+	let map = new Map();
+	
+	//Iterate over each pair of characters in the two strings
+	for (let i = 0; i < len1; i++) {
+		const c1 = str1.charAt(i);
+		const c2 = str2.charAt(i);
+		if (map.has(c1)) {				//If this character already has a mapping
+			if (map.get(c1) !== c2) {	//If this contradicts the previous mapping
+				return false;			//The mapping does not exist
+			}
+		} else {						//If this character does not have a mapping yet
+			map.set(str1.charAt(i), str2.charAt(i));	//Save what character it maps to
+		}
+	}
+	
+	//If no error in the mapping was found, the mapping exists
+	return true;
+	
+}
